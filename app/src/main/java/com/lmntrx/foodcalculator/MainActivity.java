e554077b997325a38158ed7ac22f707106cd2b66
Nihal369 <nihalismailk@gmail.com>
2017-07-26 00:22:52 +0530

    Second update

--- app/src/main/java/com/lmntrx/foodcalculator/MainActivity.java
+++ app/src/main/java/com/lmntrx/foodcalculator/MainActivity.java
@@ -2,12 +2,32 @@ package com.lmntrx.foodcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText inputField;
+    int totalFund = 600;
+    int remainingFund=600;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String totalFundString = totalFund + "";
+        TextView totalFundText = (TextView) findViewById(R.id.totalFundNum);
+        totalFundText.setText(totalFundString);
+        TextView remainingFundTextView = (TextView) findViewById(R.id.remainingFundNum);
+        String fundToDisplay=remainingFund + "";
+        remainingFundTextView.setText(fundToDisplay);
+    }
+
+    public void recordExpense(View view) {
+        remainingFund-=35;
+        TextView remainingFundTextView = (TextView) findViewById(R.id.remainingFundNum);
+        String fundToDisplay=remainingFund + "";
+        remainingFundTextView.setText(fundToDisplay);
     }
 }
